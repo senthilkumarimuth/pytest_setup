be8d77d61e077253314b346e78f6af0e6056595c
tvs_senthil <senthilkumarimuthu@gmail.com>
2023-03-18 00:06:08 +0530

    initial commit

---
+++ src/myapp.py
@@ -0,0 +1,25 @@
+
+"""
+Custom error demo
+
+"""
+
+from flask import Flask
+from submodule import sub
+
+# Setting up the app
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def addition():
+    try:
+        c = sub.divide(100, 100)
+    except Exception as e:
+        c =e
+
+    return str(c)
+
+
+if __name__ == "__main__":
+    app.run(port=500, debug=True)
---
+++ tests/unittest/test_submodule.py
@@ -0,0 +1,6 @@
+from submodule import sub
+import sys
+print(sys.path)
+
+def test_divide():
+    assert sub.divide(2, 2) == 1
